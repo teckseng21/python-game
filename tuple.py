62cb1ea09f604eafbe2b35647c3c885b08853097
teckseng21 <teckseng21@gmail.com>
2026-02-01 10:59:21 +0800

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,7 @@
+stu_details=("Ethan",90)
+
+address=('227', 'Brickfield Shelters', 'Bangalore', 'Karnataka', '562107')
+
+house_number,apartment_name,city,state,postal_code=address
+
+print (postal_code)
